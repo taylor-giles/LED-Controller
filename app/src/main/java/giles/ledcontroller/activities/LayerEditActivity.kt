ee3f3f011c83ffe09c6af81c74511739bf7410fe
taylor-giles <taylor.g1823@gmail.com>
2021-07-17 15:52:43 -0400

    Add UI for layer light selection

--- app/src/main/java/giles/ledcontroller/activities/LayerEditActivity.kt
+++ app/src/main/java/giles/ledcontroller/activities/LayerEditActivity.kt
@@ -1,12 +1,29 @@
 package giles.ledcontroller.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.RadioGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import giles.ledcontroller.R
 
 class LayerEditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_layer_edit)
+
+        val lightSelectionGroup: RadioGroup = this.findViewById(R.id.group_radio_lights_selection)
+        val alternatingLightsLayout: ConstraintLayout = this.findViewById(R.id.layout_alternating_lights_selection)
+        val advancedLightsLayout: ConstraintLayout = this.findViewById(R.id.layout_advanced_lights_selection)
+
+        lightSelectionGroup.setOnCheckedChangeListener { _, checkedID ->
+            //Remove both light selection layouts
+            alternatingLightsLayout.visibility = View.GONE
+            advancedLightsLayout.visibility = View.GONE
+            when (checkedID) {
+                R.id.radio_alternating_lights -> alternatingLightsLayout.visibility = View.VISIBLE
+                R.id.radio_advanced_lights_selection -> advancedLightsLayout.visibility = View.VISIBLE
+            }
+        }
     }
 }
